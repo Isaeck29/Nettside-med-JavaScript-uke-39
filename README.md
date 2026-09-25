4a34e8d28109e442d8d82b9a7cdea5ae4b15eade
Isaeck29 <isaeck29@innlandetfylke.no>
2026-09-25 12:57:32 +0200

    lagt inn koden fra knappeoppgaven i del 2 av oppgaven med HTML

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Nettside-med-JavaScript-uke-39
 Repository for Nettside som skal lages med Javascript for oppgaven.
+
+# Hva har jeg gjort for å lage denne nettsiden?
+ `1.` Jeg har laget filene og laget overskrift til nettsiden
+ `2.` Jeg har kopiert knapp-koden jeg brukte på oppgave 2 inn i filene til denne nettside-oppgaven. 
---
+++ script.js
@@ -0,0 +1,82 @@
+let teller = 0;
+
+function telle() {
+teller = teller + 1;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+
+function telle2() {
+teller = teller + 2;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function telle100() {
+teller = teller + 100;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function telle5() {
+teller = teller + 5;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+
+function telleminus() {
+teller = teller - 1;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+
+function telle10() {
+teller = teller + 10;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function telleminus2() {
+teller = teller - 2;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function telleminus10() {
+teller = teller - 10;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function telle1000() {
+teller = teller + 1000;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function telleminus5() {
+teller = teller - 5;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function telleminus100() {
+teller = teller - 100;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function telleminus1000 () {
+teller = teller - 1000;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function tellereset0 () {
+teller = teller * 0;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
