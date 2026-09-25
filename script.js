3af2cc98a98061ace1a8589801280fbcc21f0223
Isaeck29 <isaeck29@innlandetfylke.no>
2026-09-25 15:07:32 +0200

    lagt til tekst som beskriver koden inne i javascript-filen#

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let teller = 0;
+let teller = 0; 
 let trykkeantall = 0;
+/*variabler*/ 
 
 function telle() {
 teller = teller + 1;
@@ -8,7 +9,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;  
 console.log(teller);
 }
-
+/* funksjon for knappen som legger til 1 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 function telle2() {
 teller = teller + 2;
@@ -17,6 +18,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som legger til 2 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 function telle100() {
 teller = teller + 100;
@@ -25,6 +27,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som legger til 100 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 function telle5() {
 teller = teller + 5;
@@ -33,6 +36,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som legger til 5 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 
 function telleminus() {
@@ -42,6 +46,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som trekker fra 1 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 
 function telle10() {
@@ -51,6 +56,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som legger til 10 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 function telleminus2() {
 teller = teller - 2;
@@ -59,6 +65,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som trekker fra 2 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 function telleminus10() {
 teller = teller - 10;
@@ -67,6 +74,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som trekker fra 10 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 function telle1000() {
 teller = teller + 1000;
@@ -75,6 +83,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som legger til 1000 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 function telleminus5() {
 teller = teller - 5;
@@ -83,6 +92,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som trekker fra 5 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 function telleminus100() {
 teller = teller - 100;
@@ -91,6 +101,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som trekker fra 100 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 function telleminus1000 () {
 teller = teller - 1000;
@@ -99,6 +110,7 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som trekker fra 1000 og teller antall trykk på knappen. Console printer og ut endringen*/
 
 function tellereset0 () {
 teller = teller * 0;
@@ -107,3 +119,4 @@ document.getElementById("tallsomvises").innerHTML = teller;
 document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
+/* funksjon for knappen som resetter teller til 0 og teller antall trykk på knappen. Console printer og ut endringen*/
