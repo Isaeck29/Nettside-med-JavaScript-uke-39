fa6ea002ba305a1a58138a62bf876b7221414266
Isaeck29 <isaeck29@innlandetfylke.no>
2026-09-25 13:59:49 +0200

    lagt til en teller som teller antall totale trykk

--- script.js
+++ script.js
@@ -1,82 +1,109 @@
 let teller = 0;
+let trykkeantall = 0;
 
 function telle() {
 teller = teller + 1;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;  
 console.log(teller);
 }
 
 
 function telle2() {
 teller = teller + 2;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 function telle100() {
 teller = teller + 100;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 function telle5() {
 teller = teller + 5;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 
 function telleminus() {
 teller = teller - 1;
+trykkeantall = trykkeantall + 1; 
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 
 function telle10() {
 teller = teller + 10;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 function telleminus2() {
 teller = teller - 2;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 function telleminus10() {
 teller = teller - 10;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 function telle1000() {
 teller = teller + 1000;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 function telleminus5() {
 teller = teller - 5;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 function telleminus100() {
 teller = teller - 100;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 function telleminus1000 () {
 teller = teller - 1000;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
 
 function tellereset0 () {
 teller = teller * 0;
+trykkeantall = trykkeantall + 1;
 document.getElementById("tallsomvises").innerHTML = teller;
+document.getElementById("antallTrykket").innerHTML = trykkeantall;
 console.log(teller);
 }
